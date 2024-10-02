6c527c14d3fa2ad667bf229e09f2e5129e411190
Daniil Gentili <daniil@daniil.it>
2024-10-02 09:48:30 +0000

    Bump

--- run_all_ch.sh
+++ run_all_ch.sh
@@ -10,11 +10,11 @@ for f in Dockerfile.*; do
     type=$(echo $f | sed 's/.*\.//g')
     if [ "$1" != "" ] && [ "$1" != "$type" ]; then continue; fi
     
-    docker build . --build-arg BASE=test-$type-no-lto -t ch-test-$type
+    docker build . -f Dockerfile-clickhouse --build-arg BASE=test-$type-no-lto -t ch-test-$type
     cmds="docker run --rm -v $PWD/test-results:/var/lib/phoronix-test-suite/test-results/ -it ch-test-$type /run.sh ch-test-$type phpbench; $cmds"
 
     if grep ARCH $f; then
-        docker build . --build-arg BASE=test-$type-no-lto-native --build-arg ARCH=native -t ch-test-$type-native
+        docker build . -f Dockerfile-clickhouse --build-arg BASE=test-$type-no-lto-native --build-arg ARCH=native -t ch-test-$type-native
         cmds="docker run --rm -v $PWD/test-results:/var/lib/phoronix-test-suite/test-results/ -it ch-test-$type-native /run.sh ch-test-$type-native phpbench; $cmds"
     fi
 done
