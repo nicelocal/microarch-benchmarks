ede1bb2d6fd8b0732b5a2fd73dbff083abc5cf03
Daniil Gentili <daniil@daniil.it>
2024-10-02 09:49:02 +0000

    Bump

--- run_all_ch.sh
+++ run_all_ch.sh
@@ -10,7 +10,7 @@ for f in Dockerfile.*; do
     type=$(echo $f | sed 's/.*\.//g')
     if [ "$1" != "" ] && [ "$1" != "$type" ]; then continue; fi
     
-    docker build . -f Dockerfile-clickhouse --build-arg BASE=test-$type-no-lto -t ch-test-$type
+    docker build . -f Dockerfile-clickhouse --build-arg BASE=test-$type-no-lto-basic -t ch-test-$type
     cmds="docker run --rm -v $PWD/test-results:/var/lib/phoronix-test-suite/test-results/ -it ch-test-$type /run.sh ch-test-$type phpbench; $cmds"
 
     if grep ARCH $f; then
