c273b4b9887bd00f2a897de6ae452e55e492c631
Daniil Gentili <daniil@daniil.it>
2024-10-09 14:25:29 +0300

    Bump

--- clickhouse-install.sh
+++ clickhouse-install.sh
@@ -1,31 +1,38 @@
 #!/bin/bash -ex
 
-if which pacman &>/dev/null; then
-    pacman --noconfirm -S rustup git cmake ccache python3 ninja nasm yasm gawk lsb-release wget gnupg curl
-    export CC=clang-18
-    export CXX=clang++-18
 
-    export CFLAGS="-march=$1 -O3 -pipe -fno-plt -fexceptions \
+if [ "$CH_ARCH" != "" ]; then
+    pacman --noconfirm -S rustup git cmake ccache python3 ninja nasm yasm gawk lsb-release wget gnupg curl clang lld
+    export CC=clang
+    export CXX=clang++
+
+    export CFLAGS="-march=$CH_ARCH -O3 -pipe -fno-plt -fexceptions \
             -Wp,-D_FORTIFY_SOURCE=3 -Wformat -Werror=format-security \
             -fstack-clash-protection -fcf-protection"
     export CXXFLAGS="$CFLAGS -Wp,-D_GLIBCXX_ASSERTIONS"
     export LDFLAGS="-Wl,-O1 -Wl,--sort-common -Wl,--as-needed -Wl,-z,relro -Wl,-z,now \
             -Wl,-z,pack-relative-relocs"
     export LTOFLAGS="-flto=auto"
-    export RUSTFLAGS="target-cpu=$1"
+    export RUSTFLAGS="-C target-cpu=$CH_ARCH"
 
     rustup default stable
-    source "$HOME/.cargo/env"
 
-    git clone --recursive --shallow-submodules https://github.com/ClickHouse/ClickHouse.git
-
-    cd ClickHouse
+    cd /ClickHouse
     mkdir build
     cmake -S . -B build
     cmake --build build
     cmake --build build --target clickhouse
+    exit 0
+fi
+
+if which pacman &>/dev/null; then
+    pacman -S --noconfirm sudo
 elif which swupd &>/dev/null; then
     # Clear linux
+    swupd bundle-add sudo
 else
-    curl https://clickhouse.com/ | sh
+    apt-get install sudo
 fi
+
+curl https://clickhouse.com/ | sh
+./clickhouse install
