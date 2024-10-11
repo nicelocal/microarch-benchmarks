b12e677407e053dc1f3bdd5e382567d5a1705479
Daniil Gentili <daniil@daniil.it>
2024-10-11 13:34:44 +0300

    Cleanup

--- run_all_ch.sh
+++ run_all_ch.sh
@@ -18,15 +18,12 @@ for f in php/Dockerfile.*; do
     a=$(grep ARCH $f | sed 's/.* //')
     if [ "$a" != "" ]; then
         if [ "$f" != 'Dockerfile.alhp-v4' ]; then
-            docker build clickhouse -f clickhouse/Dockerfile-clickhouse-copy --build-arg BASE=test-$type-no-lto-native --build-arg COPY_FROM=ch-test-alhp-v4-native -t ch-test-$type-native
+            docker build clickhouse -f clickhouse/Dockerfile-clickhouse-copy --build-arg BASE=test-$type-no-lto-native --build-arg COPY_FROM=ch-test-alhp-v4-native -t ch-test-$type-native &
             cmds="docker run --rm -v $PWD/test-results:/var/lib/phoronix-test-suite/test-results/ -it ch-test-$type-native /run.sh ch-test-$type-native clickhouse; $cmds"
         fi
-
-        docker build clickhouse -f clickhouse/Dockerfile-clickhouse-build --build-arg BASE=test-$type-no-lto-native --build-arg CH_ARCH=$a -t ch-test-$type-$a
-        cmds="docker run --rm -v $PWD/test-results:/var/lib/phoronix-test-suite/test-results/ -it ch-test-$type-$a /run.sh ch-test-$type-$a clickhouse; $cmds"
     fi
 
-    docker build clickhouse -f clickhouse/Dockerfile-clickhouse --build-arg BASE=test-$type-no-lto-basic -t ch-test-$type
+    docker build clickhouse -f clickhouse/Dockerfile-clickhouse --build-arg BASE=test-$type-no-lto-basic -t ch-test-$type &
     cmds="docker run --rm -v $PWD/test-results:/var/lib/phoronix-test-suite/test-results/ -it ch-test-$type /run.sh ch-test-$type clickhouse; $cmds"
 done
 
